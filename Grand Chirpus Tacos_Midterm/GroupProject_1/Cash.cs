36a49a2ef75d5b3cd9764eed2401f8ca0e4d2ef3
chicoarellano <hi.rez.jon@gmail.com>
2019-05-20 20:34:00 -0400

    Second commit

---
+++ Grand Chirpus Tacos_Midterm/GroupProject_1/Cash.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroupProject_1
+{
+    class Cash
+    {
+        public decimal AmountGiven { get; set; }
+        public decimal Cost { get; set; }
+        public decimal AmountReturned { get; set; }
+
+        public Cash(decimal amountGiven, decimal cost/*, decimal amountReturned*/)
+        {
+            AmountGiven = amountGiven;
+            Cost = cost;
+            //AmountReturned = amountReturned;
+        }
+
+        public decimal CashBack()
+        {
+            AmountReturned = AmountGiven - Cost;
+            return AmountReturned;
+        }
+    }
+}
