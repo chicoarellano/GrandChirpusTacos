6b5fcfd619f7872864a5e3931faafcf12c894a1e
Jacob <jacobashin@gmail.com>
2019-05-30 20:12:05 -0400

    CCBranch

--- Grand Chirpus Tacos_Midterm/GroupProject_1/CreditCard.cs
+++ Grand Chirpus Tacos_Midterm/GroupProject_1/CreditCard.cs
@@ -8,7 +8,7 @@ namespace GroupProject_1
     public class Credit
     {
         public static string TakeTheCreditCard()
-        {            
+        {
             Console.WriteLine("Please enter your Credit Card Number");
             var creditCard = Console.ReadLine();
 
@@ -38,7 +38,7 @@ namespace GroupProject_1
             var expireDate = Console.ReadLine();
 
             while (!(Regex.IsMatch(expireDate, @"^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$")))
-            {
+            { 
                 Console.Clear();
                 Console.WriteLine("Invalid expiration date!");
                 Console.WriteLine();
@@ -49,47 +49,8 @@ namespace GroupProject_1
 
             Console.Clear();
 
-            return $"Paid with credit card: {creditCard}\nCVV:{CVV}\nExpires:{expireDate}";
-            
-
-
-
-
-            //if (Regex.IsMatch(creditCard, @"^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11})$"))
-            //    {
-            //        Console.WriteLine("Please enter the CVV");
-
-            //        var CVV = Console.ReadLine();
-
-            //        if (Regex.IsMatch(CVV, @"^[0-9]{3,4}$"))
-            //        {
-            //            Console.WriteLine("Please enter the expiration date");
-
-            //            var expireDate = Console.ReadLine();
-
-            //            if (Regex.IsMatch(expireDate, @"^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$"))
-            //            {
-            //                Console.WriteLine("Thank you");
-            //            }
-            //            else
-            //            {
-            //                Console.WriteLine("Hmm...There seems to be an issue with your card...");
-            //                counter++;
-            //            }
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("Hmm...There seems to be an issue with your card...");
-            //            counter++;
-            //        }
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Hmm...There seems to be an issue with your card...");
-            //        counter++;
-            //    }
-            
+            return $"Paid with credit card: XXXX-XXXX-XXXX-{creditCard.Substring(12)}";
         }
-
     }
 }
+            
